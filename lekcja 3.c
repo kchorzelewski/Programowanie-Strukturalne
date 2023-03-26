362a68278fba5dae021c4f9ee81f023a22e7b808
kchorzelewski <115093050+kchorzelewski@users.noreply.github.com>
2023-03-26 18:53:42 +0200

    Update lekcja 3.c

--- lekcja 3.c
+++ lekcja 3.c
@@ -29,19 +29,31 @@ void zad3_2_6(int n, int *w){
     printf("%d %d",*w,n);
 }
 
-int zad3_2_8(){
+int zad3_2_9(){
     int *wsk = malloc(sizeof(int));
     return wsk;
 }
 
 int zad3_2_11(int n){
-    int *tab = malloc(sizeof(double)*n);
+    int *tab = malloc(sizeof(int)*n);
     return tab;
 }
+
+int zad3_2_12(int n){
+    double *tab = malloc(sizeof(double)*n);
+    return tab;
+}
+
+int zad3_2_15(const int *n, int *m){
+    m = n;
+    return m;
+}
+
+
 
 
 int main()
 {
-    printf("%d",zad3_2_11(5));
+    printf("%d", zad3_2_15(6, 4));
     return 0;
 }
